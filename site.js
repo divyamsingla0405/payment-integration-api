1d9cee8e7c66799911d423bb9f957703a3ac472c
Divyam Singla <44860971+divyamsingla0405@users.noreply.github.com>
2021-10-17 23:12:18 +0530

    Delete site.js

--- site.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("nav").style.fontSize = "5px";}
-  // } else {
-    // document.getElementById("nav").style.fontSize = "90px";
-  // }
-}
